a18dc1fb868b79a5fc8c923d502a7a2b7780fc23
constantinelarionoff <constantinelarionoff@gmail.com>
2021-03-11 19:30:48 +0200

    renamed

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import render_template
+
+from flask import Flask, redirect, url_for, request
+app = Flask(__name__)
+
+@app.route('/')
+def text():
+    return render_template("1.html") 
+
+@app.route('/lol')
+def text2():
+    return render_template("1.html") 
+
+if __name__ == '__main__':
+    app.run(threaded=True, port=5000)
